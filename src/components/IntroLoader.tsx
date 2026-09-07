3e1c4d0b5d11161ea2ad44af53b44212f4cd7262
Ramanarayanan <ramanarayanan.gs@gmail.com>
2026-09-07 21:57:10 +0530

    Change intro loader identity to shortName (Ramanarayanan)

--- src/components/IntroLoader.tsx
+++ src/components/IntroLoader.tsx
@@ -158,7 +158,7 @@ export default function IntroLoader({ onComplete }: { onComplete: () => void }) 
           ref={identityRef}
           className="font-display font-black uppercase tracking-tighter leading-[0.85] text-white text-[clamp(2.5rem,9vw,7.5rem)] w-full max-w-5xl"
         >
-          {siteConfig.name}
+          {siteConfig.shortName}
         </div>
 
         {/* Subtitle */}
